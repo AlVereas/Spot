9c4be391af2cdbc4aedbe01e7a569e17f9a05fb2
Vereas <95543561+AlVereas@users.noreply.github.com>
2026-01-14 10:33:26 +0100

    Log request body on root endpoint

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+  console.log(req.body);
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
