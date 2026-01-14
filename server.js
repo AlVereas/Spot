fe29904c9251cfe47292a46f1e5fba676de82e18
Vereas <95543561+AlVereas@users.noreply.github.com>
2026-01-14 10:47:24 +0100

    Change GET route to POST in server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.post("/", (req, res, next) => {
   console.log(req.body);
   res.send("Hello World!");
 });
