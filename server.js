b1408f34954278b5b685b30575d30daf75e45e6c
Vereas <95543561+AlVereas@users.noreply.github.com>
2026-01-14 10:37:31 +0100

    Add /inform route and update server listening address

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.get("/inform", (req, res) => {
   console.log(req.body);
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port, '195.57.144.136', () => {
   console.log(`Listening at http://localhost:${port}`);
 });
